5a8ca466815a77cc15e32664f3dccf5c5b1c6c29
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2017-07-15 12:22:24 +0900

    separate .net3.5 and 4.6.

---
+++ Project/LambdicSql.DB2.NetFramework.3.5/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LambdicSql.DB2.NetFramework.3.5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LambdicSql.DB2.NetFramework.3.5")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("335592c4-241d-4e16-94e0-b1a23fc279b9")]
+
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
---
+++ Project/LambdicSql.DB2.NetFramework.4.6/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LambdicSql.DB2.NetFramework.4.6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LambdicSql.DB2.NetFramework.4.6")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a8bf406-258c-4f5e-8183-a06227524897")]
+
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
